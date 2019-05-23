b15832e2ee29ad6123a70123f8a74eb26137eaf2
Cchen <cchencool@gmail.com>
2019-05-23 14:36:03 +0800

    update encoding

--- code/trans.py
+++ code/trans.py
@@ -5,7 +5,7 @@ import sys
 result = ''
 trans_dic = {}
 with open('trans.json', 'r') as f:
-    trans_dic = json.load(f)#, 'utf-8')
+    trans_dic = json.load(f, encoding='utf-8')
 
 args = sys.argv
 if len(trans_dic) > 0 and len(args) > 1:
